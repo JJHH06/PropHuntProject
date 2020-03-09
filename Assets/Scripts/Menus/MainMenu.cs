ceb2ba17a7c157dca3620d8e920730105d446e86
Pablo Méndez <pablomendez778@gmail.com>
2020-03-09 00:06:36 -0600

    Tengo estéticamente la primera parte

---
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/Scripts/OnlineStuff/LobbyController.cs
@@ -0,0 +1,24 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyController : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+
+    }
+}
