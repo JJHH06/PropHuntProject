7f4f08c5cf51931aa23915e75397e1e46e08aac5
Pablo Méndez <pablomendez778@gmail.com>
2020-03-08 21:44:13 -0600

    Cree un jugador solo para hacer testing de varias camaras y demás

---
+++ Assets/Scripts/OnlineStuff/NetworkController.cs
@@ -0,0 +1,22 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkController : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Conectados a: " + PhotonNetwork.CloudRegion);
+    }
+}
