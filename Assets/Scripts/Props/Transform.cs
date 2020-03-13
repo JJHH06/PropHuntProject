dc14414a54f8c2c45a0fc67508a71bf5ee7d65dc
Orlando <orlando.osberto@gmail.com>
2020-03-13 14:40:27 -0600

    Auto stash before revert of "Importe el package que hizo J"

---
+++ Assets/Scripts/Props/Transform.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transform : MonoBehaviour
+{
+    // Start is called before the first frame update
+    RaycastHit hitInfo;
+    public float distancia = 4f;
+    void Start()
+    {
+
+    }
+
+    [System.Obsolete]
+    void FixedUpdate()
+    {
+        Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(myRay, out hitInfo, distancia))
+            {
+                if (hitInfo.collider.gameObject.CompareTag("ObjFireExtin"))
+                {
+
+
+
+
+                }
+
+            }
+
+        }
+
+    }
+}
