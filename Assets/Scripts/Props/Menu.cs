f1f719bb2cd5b464bfe0fbe530c5041b96ec462f
Jose Hurtarte <JJHH121106@gmail.com>
2020-03-15 12:39:22 -0600

    Inhabilitacionde codigos para pausa(fumada xd)

--- Assets/Scripts/Props/Menu.cs
+++ Assets/Scripts/Props/Menu.cs
@@ -8,9 +8,13 @@ public class Menu : MonoBehaviour
     // Start is called before the first frame update
     public Canvas canvas;
     private bool estado;
+    public GameObject camara;
+    public GameObject thirdPerson;
+
     void Start()
     {
         canvas.enabled = false;
+        
     }
 
     // Update is called once per frame
@@ -23,6 +27,10 @@ public class Menu : MonoBehaviour
 
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = estado;
+            
+            (camara.GetComponent("FreeLookCam") as MonoBehaviour).enabled = !estado;
+            (thirdPerson.GetComponent("vThirdPersonInput") as MonoBehaviour).enabled = !estado;
+
 
         }
     }
