3fc42778ea979e48ae6eaa326d26a2ce0e4a6b2c
Jose Hurtarte <JJHH121106@gmail.com>
2020-03-15 20:57:39 -0600

    Vida en los objetos

---
+++ Assets/Scripts/Props/PropCollision.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropCollision : MonoBehaviour
+{
+    public GameObject PropFather;
+    PropLife lifeManager = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lifeManager = PropFather.GetComponent<PropLife>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            lifeManager.VidaProp--;
+        }
+
+    }
+}
---
+++ Assets/Scripts/Props/PropLife.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropLife : MonoBehaviour
+{
+    public int VidaProp = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (VidaProp <= 0)
+        {
+            //Aqui iria lo que pasa cuando gane el hunter
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            VidaProp--;
+        }
+
+    }
+
+}
