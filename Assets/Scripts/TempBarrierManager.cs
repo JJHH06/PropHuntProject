c762f039c8b0aed8aaf8f415a46106d8e5562466
Jose Hurtarte <JJHH121106@gmail.com>
2020-03-15 18:30:21 -0600

    barrera temporal

---
+++ Assets/Scripts/TempBarrierManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempBarrierManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float tiempoBarrera = 0f;
+    public GameObject barrera;
+    private void Start()
+    {
+        if(barrera)
+         barrera.SetActive(false);
+    }
+    private void FixedUpdate()
+    {
+        tiempoBarrera += Time.fixedDeltaTime;
+        if(tiempoBarrera>= 15)
+        {
+            if (barrera)
+            {
+                barrera.SetActive(false);
+            }
+             Destroy(this);
+        }
+    }
+}
